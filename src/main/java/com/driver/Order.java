f0e52fa47716e567cd00e634a19e422faf87c217
Rahul Kishne <kishnerahul123@gmail.com>
2023-09-01 20:20:52 +0530

    7 commit

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -12,10 +12,8 @@ public class Order {
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
         this.id=id;
-        String[] time=deliveryTime.split(":");
-        String HH=String.valueOf(Integer.parseInt(time[0])/60);
-        String MM=String.valueOf(Integer.parseInt(time[1])%60);
-        this.deliveryTime=(Integer.parseInt(HH))*60 + Integer.parseInt(MM);;
+        String []time=deliveryTime.split(":");
+        this.deliveryTime=(Integer.parseInt(time[0]))*60 + Integer.parseInt(time[1]);
     }
 
     public String getId() {
